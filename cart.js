9570785e0bbfd168a38ed9535832e77be954b586
VladsD-RTV <55544051+VladsD-RTV@users.noreply.github.com>
2020-11-25 16:20:51 +0000

    Add files via upload

---
+++ cart.js
@@ -0,0 +1,22 @@
+//This function will allow the user to add to my cart
+//-------------------------------------------------------------------------------------
+
+function ItemAppear()
+{
+  //$("table").toggle(function unhide()
+//  {
+  //  $("table").display=block;
+  //}
+  //);
+  $("#tableDiv").css("visibility","visible");
+}
+
+function ItemGone()
+{
+  //$("table").toggle(function hide()
+  //{
+  //  $("table").display=none;
+  //}
+  //);
+  $("#tableDiv").hide();
+}
